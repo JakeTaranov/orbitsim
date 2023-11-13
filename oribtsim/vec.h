64a1527cdda5cae8d0ba77ed90c88554656d1b6e
Jake Taranov <48743548+JakeTaranov@users.noreply.github.com>
2023-11-13 14:00:10 -0800

    formatting vec.h

--- oribtsim/vec.h
+++ oribtsim/vec.h
@@ -8,20 +8,16 @@ public:
 	Vec();
 	Vec(float, float);
 	void print();
-
 	Vec sum(Vec);
 	Vec subtract(Vec);
 	void multiplty_scalar(float);
 	void normalize();
-
 	void add_to_x(float);
 	void add_to_y(float);
-
 	float getAngle();
 	float get_magnitude();
 	float get_x();
 	float get_y();
-
 	void set_x(float);
 	void set_y(float);
 	void reset(float, float);
