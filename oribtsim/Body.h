1d9046b3b41cab163ed21b9aea0a1957a5cc7d90
Jake Taranov <48743548+JakeTaranov@users.noreply.github.com>
2023-11-13 13:59:43 -0800

    formatting body.h

--- oribtsim/Body.h
+++ oribtsim/Body.h
@@ -18,28 +18,16 @@ class Body
 
 public:
 	Body(float mass, float radius, sf::Color col, float pos_x, float pos_y, float vel_x, float vel_y, bool drawline);
-
 	void render(sf::RenderWindow&);
-
 	void clear(sf::RenderWindow&);
-
 	void update_physics(std::vector<Body>);
-
 	void check_bounds();
-
 	void update_line();
-
 	void draw_line(sf::RenderWindow& wind);
-
 	void set_color(sf::Color);
-
 	Vec get_pos();
-
 	float get_mass();
-
 	Vec get_vel();
-
 	bool get_drawline();
-
 	sf::Color get_color();
 };
